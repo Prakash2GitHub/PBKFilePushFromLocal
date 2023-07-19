b81fffc8d7a6c8dd5fc9b3b848b755c9e513929c
Prakash <rprakash2705@gmail.com>
2023-07-19 13:53:57 +0530

    Added comment on 1:53PM

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,7 +1,7 @@
 
  class FirstJavaProgram {
        public static void main(String []args){
-           System.out.println("Prakash's first java program which pushed to github-Updated code 07/19 for jenkins to git integration with builddeployment");
+           System.out.println("Prakash's first java program which pushed to github-Updated code 07/19 for jenkins to git integration with builddeployment  - updated 19 - 1:50PM");
 }
 
 
--- Firstjava.java
+++ Firstjava.java
@@ -1,5 +1,5 @@
 public class Firstjava {
        public static void main(String []args) {
-          System.out.println("First Java Program....updated on July 19");
+          System.out.println("First Java Program....updated on July 19  - updated 19 - 1:50PM");
 }
 }
