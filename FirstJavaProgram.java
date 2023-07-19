e8ebbd18cfe3ebb951e08506af0601eb18a42fca
Prakash <rprakash2705@gmail.com>
2023-07-19 10:43:27 +0530

    Updated code to check jenkins integration

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,7 +1,7 @@
 
  class FirstJavaProgram {
        public static void main(String []args){
-           System.out.println("Prakash's first java program which pushed to github");
+           System.out.println("Prakash's first java program which pushed to github-Updated code 07/19 for jenkins to git integration with builddeployment");
 }
 
 
--- Firstjava.java
+++ Firstjava.java
@@ -1,5 +1,5 @@
 public class Firstjava {
        public static void main(String []args) {
-          System.out.println("First Java Program....");
+          System.out.println("First Java Program....updated on July 19");
 }
 }
